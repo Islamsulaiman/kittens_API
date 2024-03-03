6748f07354d2148906af2325a6cc18469a980849
Islam Sulaiman <i.sulaiman@qoyod.com>
2024-03-04 00:30:13 +0200

    feat: create the kittens controllers with the views

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,4 +1,41 @@
 class KittensController < ApplicationController
   def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def create 
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      redirect_to @kitten, notice: 'Kitten created successfully'
+    else 
+      render :new
+    end
+  end
+
+  def update 
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(kitten_params)
+      redirect_to @kitten, notice: "kitten was updated successfully"
+    else 
+      render :edit
+    end
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
